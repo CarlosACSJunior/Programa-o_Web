74d11842280dd1567960ca72bbc6d3284e46143c
Carlos Alberto Carvalho da Silva Júnior <133924173+CarlosACSJunior@users.noreply.github.com>
2023-06-09 14:46:39 -0300

    comitando

--- aula16/listar_usuario.php
+++ aula16/listar_usuario.php
@@ -14,15 +14,17 @@ if (! $conn){
 $sql = "select * from aluno";
 $query = mysqli_query($conn,$sql)
     or die (mysqli_error($conn));
-while($row = mysqli_fetch_array($query)){
-    echo $row["id"] . " " . $row["nome"] . " " . 
-    $row["idade"] . "</br>" ;
-}
-
-
-
-
-
+    echo "<table border = '1' />";
+        echo "<tr><td>ID</td><td>Nome</td><td>Idade</td><td>Ação</td></tr>"; 
+        while($row = mysqli_fetch_array($query)){
+            echo "<tr>";
+            echo "<td>" .$row["id"] ."</td>";
+            echo "<td>". $row["nome"] . "</td> "; 
+            echo "<td>". $row["idade"] . "</td>";
+            echo "<td>< a href ='deletar_aluno.php?id=.$row["id"]."'> <img width=15 height=15>src='download.png'/></a></td>";
+            echo "</tr>";
+            }
+    echo "</table>";
 mysqli_close($conn);
 
 ?>
